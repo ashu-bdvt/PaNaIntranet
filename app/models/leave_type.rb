3fd90e256b70587d4d9092a83231db583fa876b1
Ashoka Prabhakara <ashoka.p@gmail.com>
2014-01-16 19:57:26 +0530

    LeaveType Validation and buttons added

--- app/models/leave_type.rb
+++ app/models/leave_type.rb
@@ -1,2 +1,5 @@
 class LeaveType < ActiveRecord::Base
+  attr_accessible :name, :max_leaves
+  validates :name, :max_leaves, :presence => true
+  validates_numericality_of :max_leaves, :greater_than => 0
 end
