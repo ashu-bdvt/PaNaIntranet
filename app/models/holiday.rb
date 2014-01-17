fddc62ada0335b1cd8da26e796ec51a7f3514f7b
Ashoka Prabhakara <ashoka.p@gmail.com>
2014-01-17 17:43:27 +0530

    UI imporvements and validations to Holidays

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -1,2 +1,11 @@
 class Holiday < ActiveRecord::Base
+  attr_accessible :name, :date, :desciption
+  validates :name, :date, :desciption, :presence => true
+  validate :less_than_today                          
+
+  def less_than_today
+    if (date < Date.current )
+      errors.add(:date, "must be current or future date")
+    end
+  end
 end
