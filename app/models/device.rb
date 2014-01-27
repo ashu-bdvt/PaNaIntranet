c45b6a98a46f7338b9833d94bf548287dbe3a5bd
Ashoka Prabhakara <ashoka.p@gmail.com>
2014-01-27 16:47:49 +0530

    Few UI improvements and Leave type validation

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,6 +1,14 @@
 class Device < ActiveRecord::Base
   
+  validates :device_name, :serial_no, :received_date, :presence => true
+  validate :less_than_today
+  
   belongs_to :project_manager, :class_name => "Employee", :foreign_key => "owner_id"
   belongs_to :project, :class_name => "Project", :foreign_key => "project_id"
-  
+    
+  def less_than_today
+    if (received_date > Date.current )
+      errors.add(:received_date, "must be less that current date")
+    end
+  end
 end
